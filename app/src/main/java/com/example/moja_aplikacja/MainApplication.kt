d18219bb83036842d8aaf60dda6ba29b911ee59d
Alan <alankachel05@gmail.com>
2025-05-18 18:26:38 +0200

    Dodanie pliku MainApllikation

---
+++ app/src/main/java/com/example/moja_aplikacja/MainApplication.kt
@@ -0,0 +1,21 @@
+package com.example.aplikacja
+
+import TodoDatabase
+import android.app.Application
+import androidx.room.Room
+
+class MainApplication : Application() {
+
+    companion object {
+        lateinit var todoDatabase: TodoDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        todoDatabase = Room.databaseBuilder(
+            applicationContext,
+            TodoDatabase::class.java,
+            TodoDatabase.NAME
+        ).build()
+    }
+}
